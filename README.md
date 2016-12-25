25a115f1f27f8e6abb167e113c60fcae6ee4a073
Thomas Liske <thomas@fiasko-nw.net>
2016-12-25 19:08:17 +0100

    Make snmd work with recent snmd-core changes.

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 SNMD - Scotty Network Monitoring Dashboard
 ==========================================
 
-About
------
+SNMD uses HTML5 and JavaScript code to build a monitoring dashboard. Any
+data which is to be visualized needs to be published via MQTT. Widget
+libraries are required to visualize the data. A library for Nagios state
+and performance data is available.
 
-SNMD uses HTML5 and JavaScript code to build a monitoring dashboard. Any data
-which is to be visualized needs to be published via MQTT. The nagios plugin
-[nag2mqtt](liske/nag2mqtt) can be used to publish nagios performance data to
-a MQTT broker.
+The documentation of SNMD is found on [Read the Docs](https://snmd.readthedocs.io/):
+* [Architecture](https://snmd.readthedocs.io/en/latest/arch.html)
+* [Installation](https://snmd.readthedocs.io/en/latest/install.html)
 
-The following JavaScript frameworks and librarys are embedded:
-* [jQuery SVG](http://keith-wood.name/svg.html) 1.5.0
-* [Modernizr](https://modernizr.com/) 3.3.1
-* [Paho JavaScript Client](https://www.eclipse.org/paho/clients/js/) 1.0.2
-* [SVGPathData](https://github.com/nfroidure/SVGPathData) 1.0.3
-
-The following resources are included via `bower`:
+The following JavaScript libraries are included via `bower`:
 * [Font Awesome](http://fontawesome.io/) 4.7.0
-* [Open Sans @font-face kit](https://github.com/FontFaceKit/open-sans) 1.4.2
 * [jQuery](https://jquery.com/) 2.2.4
-* [Moment.js](http://momentjs.com/) 2.15.2
+* [js-cookie](https://github.com/js-cookie/js-cookie) 2.1.3
+* [js-logger](https://github.com/jonnyreeves/js-logger) 1.3.0
+* [Open Sans @font-face kit](https://github.com/FontFaceKit/open-sans) 1.4.2
 * [qTip²](http://qtip2.com/) 3.0.3
 * [sprintf.js](https://github.com/alexei/sprintf.js) 1.0.3
+
+The following JavaScript libraries are embedded:
+* [jQuery SVG](http://keith-wood.name/svg.html) 1.5.0
+* [Paho JavaScript Client](https://www.eclipse.org/paho/clients/js/) 1.0.2
+* [SVGPathData](https://github.com/nfroidure/SVGPathData) 1.0.3
---
+++ js/Boot.js
@@ -0,0 +1,74 @@
+/*
+SNMD - Scotty Network Management Dashboard
+  https://github.com/DE-IBH/snmd/
+
+Authors:
+  Thomas Liske <liske@ibh.de>
+
+Copyright Holder:
+  2012 - 2013 (C) Thomas Liske [https://fiasko-nw.net/~thomas/]
+  2014 - 2016 (C) IBH IT-Service GmbH [https://www.ibh.de/]
+
+License:
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this package; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+/*jslint
+    devel: true,
+    plusplus: true,
+    vars: true
+*/
+
+/*global
+    require
+*/
+
+require.config({
+    baseUrl: "blib",
+    paths: {
+        "jquery": "jquery/dist/jquery",
+        "js-logger": "js-logger/src/logger.min",
+        "snmd-core": "snmd-core/src",
+        "snmd-widgets-nagios": "snmd-widgets-nagios/src"
+    },
+    enforceDefine: true,
+    urlArgs: "v=" + (new Date()).getTime()
+});
+
+require(["jquery", "js-logger"], function ($, Logger) {
+    'use strict';
+
+    Logger.useDefaults();
+    Logger.setLevel(Logger.INFO);
+
+    $.ajax({
+        url: 'config.json',
+        dataType: 'json',
+        success: function (config, textStatus) {
+            require(["snmd-core/Main"], function (Main) {
+                var main = new Main(config);
+            });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            var msg = "Failed to config file due to " + textStatus + ": " + errorThrown;
+            console.error(msg);
+            $('body').text(msg).css({
+                'font-size': 'larger',
+                'font-weight': 'bold',
+                color: 'red'
+            });
+        }
+    });
+});
